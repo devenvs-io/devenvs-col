06f30868d4d697217a69a9fb71f4aba91e93270e
Devarsh <devarsh@devenvs.io>
2025-01-11 00:26:14 +0530

    removed-debugging-echo-statements

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 
 echo "Hello $1 $2 $3 $4 $5 $6 $7 $8 $9"
-echo "Hello from the 2nd echo statement!"
+# echo "Hello from the 2nd echo statement!"
 
 # Constructing JSON payload
 json_payload=$(cat <<EOF
@@ -22,7 +22,7 @@ EOF
 api_url="$9"
 echo "******"
 echo $json_payload
-echo "Hi, I am here for debugging purpose!"
+# echo "Hi, I am here for debugging purpose!"
 
 # curl --location "$9" \
 # --header "Authorization: Bearer $1" \
@@ -41,12 +41,12 @@ echo "Hi, I am here for debugging purpose!"
 
 
 # Using curl to send a POST request with the JSON payload
-echo "Hello we're about to hit the post request!"
+# echo "Hello we're about to hit the post request!"
 curl -X POST "$api_url" \
         -H "Content-Type: application/json" \
         -H "x-api-key: $1" \
         -d "$json_payload" 
-echo "We have hit the post request!"
+# echo "We have hit the post request!"
 
 time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
